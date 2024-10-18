bb8a24600d8f63b9560484a770913dd2c4adc818
abolfazl <abolfazl.shirmahalei96@gmail.com>
2024-10-18 19:54:48 +0100

    relocate milvus runner

--- applications/config.py
+++ applications/config.py
@@ -1,6 +1,6 @@
 import os
 
 DATA_BASE_DEFAULT_PATH = os.path.expanduser("~/database")
-DATA_BASE_RUNNER_PATH = os.path.expanduser("~/Milvus/milvus_runner.sh")
+DATA_BASE_RUNNER_PATH = os.path.expanduser("~/Milvus/applications/milvus_runner.sh")
 DATA_BASE_CONTAINER_NAME = "milvus-standalone"
 DATA_BASE_PORT = 19530
